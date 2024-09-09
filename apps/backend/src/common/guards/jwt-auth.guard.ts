6b984de3015623cda28563085abed5b844957872
Mikołaj <gawrmiko@gmail.com>
2024-09-09 23:19:22 +0200

    feat: update jwt-auth.guard.ts

--- apps/backend/src/common/guards/jwt-auth.guard.ts
+++ apps/backend/src/common/guards/jwt-auth.guard.ts
@@ -4,16 +4,16 @@ import { AuthGuard } from "@nestjs/passport";
 import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard("jwt") implements CanActivate {
+export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor(private reflector: Reflector) {
     super();
   }
 
   canActivate(context: ExecutionContext) {
-    const isPublic = this.reflector.get<boolean>(
-      IS_PUBLIC_KEY,
-      context.getHandler()
-    );
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
     if (isPublic) {
       return true;
     }
