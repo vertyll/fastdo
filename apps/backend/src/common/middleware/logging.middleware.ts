02d23c5951353c990a62f1cd146e1e41c536ee7d
Mikołaj Gawron <gawrmiko@gmail.com>
2024-09-18 20:08:16 +0200

    feat: add unique request id

--- apps/backend/src/common/middleware/logging.middleware.ts
+++ apps/backend/src/common/middleware/logging.middleware.ts
@@ -1,11 +1,14 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware } from "@nestjs/common";
 
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    console.time('Request-response time');
+    const requestId = Date.now();
+    console.time(`Request-response time ${requestId}`);
 
-    res.on('finish', () => console.timeEnd('Request-response time'));
+    res.on("finish", () =>
+      console.timeEnd(`Request-response time ${requestId}`)
+    );
     next();
   }
 }
