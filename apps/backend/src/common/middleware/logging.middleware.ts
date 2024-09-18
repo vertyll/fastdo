0d77a03e4b129fc9af006b708845211f051afffe
Mikołaj Gawron <gawrmiko@gmail.com>
2024-09-18 20:20:10 +0200

    feat: add additional random number to requestId

--- apps/backend/src/common/middleware/logging.middleware.ts
+++ apps/backend/src/common/middleware/logging.middleware.ts
@@ -3,7 +3,7 @@ import { Injectable, NestMiddleware } from "@nestjs/common";
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    const requestId = Date.now();
+    const requestId = `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
     console.time(`Request-response time ${requestId}`);
 
     res.on("finish", () =>
