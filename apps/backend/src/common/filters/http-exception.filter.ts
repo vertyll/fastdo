09a562b8e971bce2dec7d5d8a0b85aee400cd5ef
Mikołaj Gawron <gawrmiko@gmail.com>
2024-09-20 19:38:51 +0200

    feat: improve exception filter
    add info about fields error

--- apps/backend/src/common/filters/http-exception.filter.ts
+++ apps/backend/src/common/filters/http-exception.filter.ts
@@ -17,12 +17,13 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest<FastifyRequest>();
     const status = exception.getStatus();
 
-    const responseBody = {
+    const responseBody: any = {
       statusCode: status,
       timestamp: new Date().toISOString(),
       path: request.url,
       method: request.method,
-      message: exception.message || null,
+      message: this.formatGeneralMessage(exception),
+      fields: this.extractFields(exception),
     };
 
     this.logger.error(
@@ -32,5 +33,40 @@ export class HttpExceptionFilter implements ExceptionFilter {
     );
 
     response.status(status).send(responseBody);
+  }
+
+  private formatGeneralMessage(exception: HttpException): string {
+    const response = exception.getResponse();
+    if (typeof response === "string") {
+      return response;
+    } else if (
+      typeof response === "object" &&
+      response.hasOwnProperty("message")
+    ) {
+      if (Array.isArray(response["message"])) {
+        return "Validation failed";
+      } else {
+        return response["message"];
+      }
+    } else {
+      return exception.message || "An error occurred";
+    }
+  }
+
+  private extractFields(exception: HttpException): any {
+    const response = exception.getResponse();
+    if (typeof response === "object" && response.hasOwnProperty("message")) {
+      if (Array.isArray(response["message"])) {
+        return response["message"]
+          .map((msg: any) => {
+            if (typeof msg === "object" && msg.field) {
+              return { field: msg.field, errors: msg.errors || [] };
+            }
+            return null;
+          })
+          .filter((msg: any) => msg !== null);
+      }
+    }
+    return null;
   }
 }
