2bcbe8ab8b4dc5a833c23c12ae71981a34bcb055
Mikolaj Gawron <gawrmiko@gmail.com>
2024-09-25 17:43:21 +0200

    style: add border radius and use tailwind css

--- apps/frontend/src/app/shared/components/info-panel.component.ts
+++ apps/frontend/src/app/shared/components/info-panel.component.ts
@@ -10,35 +10,27 @@ import {
   standalone: true,
   imports: [NgIconComponent],
   providers: [provideIcons({ bootstrapArrowLeft, bootstrapArrowRight })],
-  styles: [
-    `
-      .info-panel {
-        @apply fixed bottom-0 right-0 bg-gray-800 text-white p-4 transition-transform transform translate-x-full;
-        width: calc(100% - 40px);
-      }
-      .info-panel.open {
-        @apply translate-x-0;
-      }
-      .toggle-button {
-        @apply fixed bottom-0 right-0 bg-orange-500 text-white p-4 cursor-pointer flex items-center justify-center;
-        z-index: 10;
-        width: 40px;
-        height: 40px;
-        user-select: none;
-      }
-    `,
-  ],
   template: `
     @if (isLoggedIn()) {
       <div>
-        <div class="toggle-button" (click)="togglePanel()">
+        <!-- Przycisk przełączania -->
+        <div
+          class="fixed bottom-0 right-0 bg-orange-500 text-white p-2 cursor-pointer flex items-center justify-center z-10 w-10 h-10 user-select-none rounded-tl-md"
+          (click)="togglePanel()"
+        >
           @if (!panelOpen) {
             <ng-icon name="bootstrapArrowLeft"></ng-icon>
           } @else {
             <ng-icon name="bootstrapArrowRight"></ng-icon>
           }
         </div>
-        <div class="info-panel" [class.open]="panelOpen">
+        
+        <!-- Panel informacyjny -->
+        <div
+          class="fixed bottom-0 right-0 bg-gray-800 text-white p-4 transition-transform transform w-[calc(100%-40px)] rounded-tl-md duration-300 ease-in-out"
+          [class.translate-x-0]="panelOpen"
+          [class.translate-x-full]="!panelOpen"
+        >
           <div>
             <b>User roles</b>:
             <span class="text-green-500"> {{ userRolesString }} </span>
