eec325cecd939ff78564485d15eaf13c8d086980
Mikołaj Gawron <gawrmiko@gmail.com>
2025-01-11 10:51:49 +0100

    style: change info-panel style

--- apps/frontend/src/app/shared/components/organisms/info-panel.component.ts
+++ apps/frontend/src/app/shared/components/organisms/info-panel.component.ts
@@ -1,12 +1,23 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { heroArrowLeft, heroArrowRight } from '@ng-icons/heroicons/outline';
+import {
+  heroArrowLeft,
+  heroArrowRight,
+  heroEllipsisHorizontal,
+  heroChevronUp
+} from '@ng-icons/heroicons/outline';
 import { TranslateModule } from '@ngx-translate/core';
+
 @Component({
-    selector: 'app-info-panel',
-    imports: [NgIconComponent, TranslateModule],
-    providers: [provideIcons({ heroArrowLeft, heroArrowRight })],
-    template: `
+  selector: 'app-info-panel',
+  imports: [NgIconComponent, TranslateModule],
+  providers: [provideIcons({
+    heroArrowLeft,
+    heroArrowRight,
+    heroEllipsisHorizontal,
+    heroChevronUp
+  })],
+  template: `
     @if (isLoggedIn()()) {
       <div>
         <div
@@ -20,27 +31,100 @@ import { TranslateModule } from '@ngx-translate/core';
           }
         </div>
         <div
-          class="fixed bottom-0 right-0 bg-gray-800 text-white p-4 transition-transform transform w-[calc(100%-40px)] rounded-tl-md duration-300 ease-in-out"
+          class="fixed bottom-0 right-0 bg-gray-800 text-white p-4 transition-transform transform w-[calc(100%-40px)] h-10 rounded-tl-md duration-300 ease-in-out flex items-center gap-6 overflow-x-auto pr-8"
           [class.translate-x-0]="panelOpen()"
           [class.translate-x-full]="!panelOpen()"
         >
-          <div>
-            <b>{{ 'InfoPanel.userRoles' | translate }}</b
-            >:
-            <span class="text-green-500"> {{ userRolesString() }} </span>
+          <div class="flex items-center min-w-fit">
+            <b>{{ 'InfoPanel.userRoles' | translate }}</b>:
+            @if (expandedSection() === 'roles') {
+              <span class="text-green-500 ml-2 break-all">
+                @if (userRolesString().length > maxVisibleChars) {
+                  <button
+                    class="mr-1 p-1 hover:bg-gray-700 rounded"
+                    (click)="toggleSection('')"
+                  >
+                    <ng-icon name="heroChevronUp" class="text-sm"></ng-icon>
+                  </button>
+                }
+                {{ userRolesString() }}
+              </span>
+            } @else {
+              <span class="text-green-500 ml-2 truncate max-w-[100px]">
+                {{ userRolesString() }}
+              </span>
+              @if (userRolesString().length > maxVisibleChars) {
+                <button class="ml-2" (click)="toggleSection('roles')">
+                  <ng-icon name="heroEllipsisHorizontal" class="text-sm"></ng-icon>
+                </button>
+              }
+            }
           </div>
-          <div>
-            <b> {{ 'InfoPanel.currentTime' | translate }} </b>:
-            <span class="text-green-500"> {{ currentTime() }} </span>
+
+          <div class="flex items-center min-w-fit">
+            <b>{{ 'InfoPanel.currentTime' | translate }}</b>:
+            @if (expandedSection() === 'time') {
+              <span class="text-green-500 ml-2 break-all">
+                @if (currentTime().length > maxVisibleChars) {
+                  <button
+                    class="mr-1 p-1 hover:bg-gray-700 rounded"
+                    (click)="toggleSection('')"
+                  >
+                    <ng-icon name="heroChevronUp" class="text-sm"></ng-icon>
+                  </button>
+                }
+                {{ currentTime() }}
+              </span>
+            } @else {
+              <span class="text-green-500 ml-2 truncate max-w-[100px]">
+                {{ currentTime() }}
+              </span>
+              @if (currentTime().length > maxVisibleChars) {
+                <button class="ml-2" (click)="toggleSection('time')">
+                  <ng-icon name="heroEllipsisHorizontal" class="text-sm"></ng-icon>
+                </button>
+              }
+            }
           </div>
-          <div>
-            <b> {{ 'InfoPanel.browserInfo' | translate }} </b>:
-            {{ browserInfo() }}
+
+          <div class="flex items-center min-w-fit">
+            <b>{{ 'InfoPanel.browserInfo' | translate }}</b>:
+            @if (expandedSection() === 'browser') {
+              <span class="text-green-500 ml-2 break-all">
+                @if (browserInfo().length > maxVisibleChars) {
+                  <button
+                    class="mr-1 p-1 hover:bg-gray-700 rounded"
+                    (click)="toggleSection('')"
+                  >
+                    <ng-icon name="heroChevronUp" class="text-sm"></ng-icon>
+                  </button>
+                }
+                {{ browserInfo() }}
+              </span>
+            } @else {
+              <span class="text-green-500 ml-2 truncate max-w-[100px]">
+                {{ browserInfo() }}
+              </span>
+              @if (browserInfo().length > maxVisibleChars) {
+                <button class="ml-2" (click)="toggleSection('browser')">
+                  <ng-icon name="heroEllipsisHorizontal" class="text-sm"></ng-icon>
+                </button>
+              }
+            }
           </div>
         </div>
       </div>
     }
-  `
+  `,
+  styles: [`
+    :host {
+      .truncate {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+  `]
 })
 export class InfoPanelComponent {
   readonly panelOpen = input<boolean>(false);
@@ -49,4 +133,11 @@ export class InfoPanelComponent {
   readonly currentTime = input<string>('');
   readonly browserInfo = input<string>('');
   readonly isLoggedIn = input.required<() => boolean>();
+
+  protected readonly maxVisibleChars = 20;
+  protected expandedSection = signal<string>('');
+
+  protected toggleSection(section: string): void {
+    this.expandedSection.set(this.expandedSection() === section ? '' : section);
+  }
 }
