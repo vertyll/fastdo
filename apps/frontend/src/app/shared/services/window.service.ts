e0c9653337a2e9a920005f652cb7fc2917a290ad
Mikołaj Gawron <gawrmiko@gmail.com>
2025-01-12 13:43:06 +0100

    feat: add window service

---
+++ apps/frontend/src/app/shared/services/window.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WindowService {
+  private readonly windowWidth = signal(window.innerWidth);
+
+  constructor() {
+    window.addEventListener('resize', () => {
+      this.windowWidth.set(window.innerWidth);
+    });
+  }
+
+  getWindowWidth() {
+    return this.windowWidth.asReadonly();
+  }
+}
