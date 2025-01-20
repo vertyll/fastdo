4d1e97588c5f70c3ce00f10133538b571b345b79
Mikołaj Gawron <gawrmiko@gmail.com>
2025-01-20 23:02:43 +0100

    chore: bump nest version from 10 to 11

--- apps/backend/src/auth/strategies/jwt.strategy.ts
+++ apps/backend/src/auth/strategies/jwt.strategy.ts
@@ -1,20 +1,25 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from '../../users/entities/user.entity';
 import { UsersService } from '../../users/users.service';
-import {User} from "../../users/entities/user.entity";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly usersService: UsersService) {
+    const jwtSecret = process.env.JWT_SECRET;
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET is not defined');
+    }
+
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: jwtSecret,
     });
   }
 
-  async validate(payload: any): Promise<User & { roles: string[] }> {
+  async validate(payload: any): Promise<User & { roles: string[]; }> {
     const user = await this.usersService.findOne(payload.sub);
     if (!user || !user.isActive || !user.isEmailConfirmed) {
       throw new UnauthorizedException();
