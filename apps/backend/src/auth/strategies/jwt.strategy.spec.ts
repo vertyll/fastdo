7180bf74c6eb875784bcf14ee95679128785ec82
Mikołaj Gawron <gawrmiko@gmail.com>
2025-01-21 19:24:49 +0100

    chore: update test

--- apps/backend/src/auth/strategies/jwt.strategy.spec.ts
+++ apps/backend/src/auth/strategies/jwt.strategy.spec.ts
@@ -5,44 +5,47 @@ import { JwtStrategy } from './jwt.strategy';
 
 describe('JwtStrategy', () => {
   let jwtStrategy: JwtStrategy;
-  let usersService: Partial<UsersService>;
-  let configService: Partial<ConfigService>;
+  let usersService: jest.Mocked<UsersService>;
+  let configService: jest.Mocked<ConfigService>;
+
+  const mockUser = {
+    id: 1,
+    email: 'test@test.com',
+    isActive: true,
+    isEmailConfirmed: true,
+    password: 'hashedPassword',
+    dateCreation: new Date(),
+    dateModification: null,
+    userRoles: [],
+    confirmationToken: 'token',
+    confirmationTokenExpiry: new Date(),
+  };
 
   beforeEach(() => {
     usersService = {
       findOne: jest.fn(),
-    };
+    } as unknown as jest.Mocked<UsersService>;
 
     configService = {
-      get: jest.fn().mockReturnValue('test-secret'),
-    };
+      get: jest.fn().mockImplementation((key: string) => {
+        switch (key) {
+          case 'app.security.jwt.secret':
+            return 'test-secret';
+          default:
+            return null;
+        }
+      }),
+    } as unknown as jest.Mocked<ConfigService>;
 
     jwtStrategy = new JwtStrategy(
-      usersService as UsersService,
-      configService as ConfigService,
+      usersService,
+      configService,
     );
-  });
-
-  afterEach(() => {
-    delete process.env.JWT_SECRET;
   });
 
   it('should validate and return a user if valid payload is provided', async () => {
     const payload = { sub: 1, roles: ['admin'] };
-    const mockUser = {
-      id: 1,
-      email: 'test@test.com',
-      isActive: true,
-      isEmailConfirmed: true,
-      password: 'hashedPassword',
-      dateCreation: new Date(),
-      dateModification: null,
-      userRoles: [],
-      confirmationToken: 'token',
-      confirmationTokenExpiry: new Date(),
-    };
-
-    jest.spyOn(usersService, 'findOne').mockResolvedValue(mockUser);
+    usersService.findOne.mockResolvedValue(mockUser);
 
     const result = await jwtStrategy.validate(payload);
 
@@ -52,28 +55,17 @@ describe('JwtStrategy', () => {
 
   it('should throw an UnauthorizedException if user is not found', async () => {
     const payload = { sub: 1, roles: ['admin'] };
-
-    jest.spyOn(usersService, 'findOne').mockResolvedValue(null);
+    usersService.findOne.mockResolvedValue(null);
 
     await expect(jwtStrategy.validate(payload)).rejects.toThrow(UnauthorizedException);
   });
 
   it('should throw an UnauthorizedException if user is inactive', async () => {
     const payload = { sub: 1, roles: ['admin'] };
-    const mockUser = {
-      id: 1,
-      email: 'test@test.com',
+    usersService.findOne.mockResolvedValue({
+      ...mockUser,
       isActive: false,
-      isEmailConfirmed: true,
-      password: 'hashedPassword',
-      dateCreation: new Date(),
-      dateModification: null,
-      userRoles: [],
-      confirmationToken: 'token',
-      confirmationTokenExpiry: new Date(),
-    };
-
-    jest.spyOn(usersService, 'findOne').mockResolvedValue(mockUser);
+    });
 
     await expect(jwtStrategy.validate(payload)).rejects.toThrow(UnauthorizedException);
   });
